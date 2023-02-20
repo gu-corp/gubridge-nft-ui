ff2a6b091bca071d277b0706bd6281330ded8ace
cuonghx-gutech <cuonghx@gu-tech.com>
2023-02-20 16:59:39 +0700

    feat: remove create nft page

--- packages/dapp/src/Routes.jsx
+++ packages/dapp/src/Routes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { About } from './pages/About';
-import { CreateNFT } from './pages/CreateNFT';
 import { History } from './pages/History';
 import { Home } from './pages/Home';
 
@@ -10,7 +9,6 @@ export const Routes = () => (
   <Switch>
     <Route exact path="/bridge" component={Home} />
     <Route exact path="/history" component={History} />
-    <Route exact path="/create" component={CreateNFT} />
     <Route exact path="/about" component={About} />
     <Redirect to="/bridge" />
   </Switch>
--- packages/dapp/src/pages/CreateNFT.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import { Create } from '../components/create/Create';
-
-export const CreateNFT = ({ location }) => <Create />;
