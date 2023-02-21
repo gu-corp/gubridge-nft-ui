acd43c2c2e78fc2e3442dfd4939ce0291a73f0f6
cuonghx-gutech <cuonghx@gu-tech.com>
2023-02-21 11:34:38 +0700

    chore: get version app from package json & remove unnecesary workflow

--- packages/dapp/src/pages/About.jsx
+++ packages/dapp/src/pages/About.jsx
@@ -2,5 +2,7 @@ import { Text } from '@chakra-ui/react';
 import React from 'react';
 
 export const About = () => (
-  <Text>Version: {process.env.REACT_APP_VERSION}</Text>
+  <Text fontSize="4xl" fontWeight="bold" marginTop="10">
+    Version: {process.env.REACT_APP_VERSION}
+  </Text>
 );
