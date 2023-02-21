4027bc9dea04e5e7b9e714401e9bd9f40cabfd2e
cuonghx-gutech <cuonghx@gu-tech.com>
2023-02-21 17:42:31 +0700

    chore: get version app from package json

--- packages/dapp/src/pages/About.jsx
+++ packages/dapp/src/pages/About.jsx
@@ -2,12 +2,14 @@ import { Text } from '@chakra-ui/react';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import packageJson from '../../package.json';
+
 export const About = () => {
   const { t } = useTranslation();
 
   return (
     <Text fontSize="4xl" fontWeight="bold" marginTop="10">
-      {t('version')}: {process.env.REACT_APP_VERSION}
+      {t('version')}: {packageJson.version}
     </Text>
   );
 };
