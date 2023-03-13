e1108237a6b2a526460f90ddd2a888b5c66fd6aa
cuonghx-gutech <cuonghx@gu-tech.com>
2023-03-13 14:55:30 +0700

    chore: about page sperate from layout

--- packages/dapp/src/App.jsx
+++ packages/dapp/src/App.jsx
@@ -3,7 +3,6 @@ import 'focus-visible/dist/focus-visible';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { css, Global } from '@emotion/react';
 import ErrorBoundary from 'components/common/ErrorBoundary';
-import { Layout } from 'components/common/Layout';
 import { SettingsProvider } from 'contexts/SettingsContext';
 import { Web3Provider } from 'contexts/Web3Context';
 import React from 'react';
@@ -39,9 +38,7 @@ export const App = () => (
         <Router>
           <SettingsProvider>
             <Web3Provider>
-              <Layout>
-                <Routes />
-              </Layout>
+              <Routes />
             </Web3Provider>
           </SettingsProvider>
         </Router>
